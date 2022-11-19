d8d938925804c39a6562794480a81bb52ac8f266
Mike Key <mike@1337hero.com>
2022-11-19 14:21:16 -0700

    moving towards new site

--- gulpfile.js
+++ gulpfile.js
@@ -1,128 +1,93 @@
-// Mike's Gulp Workflow
+// Mike's MK3Y Jekyll + Gulp Workflow
 'use strict';
 
+// Plugins
+const   gulp = require('gulp'),
+        browserSync = require('browser-sync').create(),
+           partials = require('gulp-inject-partials'),
+            htmlmin = require('gulp-htmlmin'),
+             concat = require('gulp-concat'),
+             rename = require('gulp-rename'),
+              clean = require('gulp-clean-css'),
+               maps = require('gulp-sourcemaps'),
+               sass = require('gulp-sass')(require('sass'));
 
-// Require our stuff
-var    gulp = require('gulp'),
-browserSync = require('browser-sync').create(),
-   cleanCSS = require('gulp-clean-css'),
-   imagemin = require('gulp-imagemin'),
-     concat = require('gulp-concat'),
-     uglify = require('gulp-uglify'),
-     rename = require('gulp-rename'),
-     deploy = require('gulp-gh-pages'),
-      gutil = require('gulp-util'),
-       sass = require('gulp-sass'),
-       maps = require('gulp-sourcemaps'),
-         cp = require('child_process');
-
-
-// Set those paths
-const base_path = './',
-            src = base_path + '_dev',
-           dist = base_path + 'assets',
-          paths = {
-              js: src + '/js/*.js' ,
-             img: src + '/img/*',
-            scss: src + '/sass/app.scss',
-          jekyll: ['*.html', '*.md', '_posts/*.md', '_layouts/*.html', '_includes/*.html'],
-        critical: src + '/sass/critical.scss'
-          };
+// File Paths
+const base = './',
+       src = base + 'dev',
+      dest = base + 'public',
+     paths = {
+          js: src + '/js/app.js' ,
+         img: src + '/img/*',
+        html: src + '/html/*.html',
+        scss: src + '/sass/app.scss'
+     };
 
 
-// Do important stuff with SASS
-gulp.task('makeCRITICAL', () => {
-    return gulp.src(paths.critical)
-        .pipe(sass())
-        .pipe(cleanCSS({compatibility: '*'}, {level: '2'}))
-        .pipe(rename({
-            basename: 'criticalCSS',
-            extname: '.php'
-          }))
-        .pipe(gulp.dest('_inc/'))
-        .pipe(browserSync.stream());
-});
-
-
-// Do stuff with SASS
-gulp.task('makeCSS', () => {
+// SASS => CSS
+function css(done) {
     return gulp.src(paths.scss)
         .pipe(maps.init())
-        .pipe(sass())
-        .pipe(cleanCSS({compatibility: '*'}, {level: '2'}))
-        .pipe(rename('styles.min.css'))
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest(dist))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(clean()) // for minifying the file
+        .pipe(rename('style.css'))
+        .pipe(maps.write())
+        .pipe(gulp.dest(dest + '/css'))
         .pipe(browserSync.stream());
-});
+    done();
+}
 
-
-// Do stuff with Javascript
-gulp.task('makeJS', () => {
-    return gulp.src(paths.js)
-        .pipe(maps.init())
-        .pipe(concat('scripts.js'))
-        .pipe(uglify())
-        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-        .pipe(rename('scripts.min.js'))
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest(dist))
+// HTML Partials
+function html(done) {
+    return gulp.src(paths.html)
+        .pipe(partials({
+            start: '<% {{path}} ',
+              end: '%>',
+           removeTags: true
+           }))
+        .pipe(htmlmin({
+           collapseWhitespace: true, 
+           removeComments: true
+        }))
+        .pipe(gulp.dest(dest))
         .pipe(browserSync.stream());
-});
 
+    done();
+}
 
-// Let's compress some images
-gulp.task('compressIMG', () => {
-    return gulp.src(paths.img)
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.jpegtran({progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe(gulp.dest('images/'));
-});
+// Watch for changes & do stuff
+function watchFiles() {
+    gulp.watch(paths.scss + '**', gulp.series(css, reload));
+    gulp.watch(paths.html + '**', gulp.series(html, reload));
+}
 
-
-// Build Jekyll
-gulp.task('jekyll-build', (code) => {
-  return cp.spawn('jekyll', ['build', '--incremental'], { stdio: 'inherit' }) // Adding incremental reduces build time.
-    .on('error', (error) => gutil.log(gutil.colors.red(error.message)))
-    .on('close', code);
-});
-
-
-// Rebuild Jekyll with Delay
-gulp.task('jekyll-rebuild', ['jekyll-build'], () => {
-    browserSync.reload({reloadDelay: 5000});
-});
-
-
-// Setup a Gulp Server with Browser Sync
-gulp.task('serve', ['makeCSS', 'makeJS', 'jekyll-build'], () => {
+// Run a server 
+function server() {
     browserSync.init({
-        server: './public_html/',
-        port: 3000
+        server: {
+             baseDir: "./public"
+        }
     });
-    gulp.watch(src + '/sass/*.scss', ['makeCSS']).on('change', browserSync.reload);
-    gulp.watch(src + '/js/*.js', ['makeJS']).on('change', browserSync.reload);
-    gulp.watch(paths.jekyll, ['jekyll-rebuild']);
-});
+}
+
+// Reload my Browser
+function reload(done) {
+    browserSync.reload();
+    done();
+}
 
 
-// DEPLOY TO GITHUB
-gulp.task('push', () => {
-  return gulp.src("./**/*")
-    .pipe(deploy())
-});
+// Confirm project ready to sync
+function message(done) {
+    return gulp.src(src)
+    .pipe(prompt.confirm('Are you sure you are done with the project?'))
+    done();
+}
 
+// Complex tasks
+const dev = gulp.parallel(server, watchFiles);
+const build = gulp.series(html,css);
 
-// Do this stuff by default
-gulp.task('default', ['serve']);
-gulp.task('build', ['makeCSS', 'makeJS', 'compressIMG', 'jekyll-build']);
-gulp.task('deploy', ['build', 'push']);
+// Export tasks
+exports.default = dev;
+exports.build = build;
